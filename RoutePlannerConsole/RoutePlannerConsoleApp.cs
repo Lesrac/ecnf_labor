80f477d14c3156509cfe43f75463c22e2d142501
Stolz77 <stolz77@bluewin.ch>
2014-09-16 16:10:26 +0200

    initial commit

---
+++ RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
+{
+    class RoutePlannerConsoleApp
+    {
+        static void Main(string[] args)
+        {
+            /* FILE VERSION
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            string version = fvi.FileVersion;
+            Console.WriteLine("Welcome to RoutePlanner ({0})", version);
+             */
+            Version version = Assembly.GetEntryAssembly().GetName().Version;
+            Console.WriteLine("Welcome to RoutePlanner ({0})", version);
+            Console.ReadKey();
+        }
+    }
+}
