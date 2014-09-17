e128466497d3ddb5dd847cb0347b6583b7d8e14d
Roman <roman.holzner@students.fhnw.ch>
2014-09-17 22:14:14 +0200

    Code Cleanup

--- RoutePlannerConsole/RoutePlannerConsoleApp.cs
+++ RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -7,12 +7,6 @@
     {
         static void Main(string[] args)
         {
-            /* FILE VERSION
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
-            string version = fvi.FileVersion;
-            Console.WriteLine("Welcome to RoutePlanner ({0})", version);
-             */
             Version version = Assembly.GetEntryAssembly().GetName().Version;
             Console.WriteLine("Welcome to RoutePlanner ({0})", version);
             Console.ReadKey();
