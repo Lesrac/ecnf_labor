3bae1bf03d487c3ecd68af2a726208adb588c2e7
Stolz77 <stolz77@bluewin.ch>
2014-09-23 14:56:57 +0200

    TestLocal

--- RoutePlannerConsole/RoutePlannerConsoleApp.cs
+++ RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -11,6 +11,7 @@
             Version version = Assembly.GetEntryAssembly().GetName().Version;
             Console.WriteLine("Welcome to RoutePlanner ({0})", version);
 
+
             var wayPoint = new WayPoint("Windisch", 47.479319847061966, 8.212966918945312);
             Console.WriteLine("{0}: {1}/{2}", wayPoint.Name, wayPoint.Latitude, wayPoint.Longitude);
 
