48e549c195e3c37fb47a6097efb16bc032b7e98c
Stolz77 <stolz77@bluewin.ch>
2014-09-27 23:13:57 +0200

    Aufgabe 2.1
    Signed-off-by: Stolz77 <stolz77@bluewin.ch>

--- RoutePlannerConsole/RoutePlannerConsoleApp.cs
+++ RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -12,8 +12,11 @@
             Console.WriteLine("Welcome to RoutePlanner ({0})", version);
 
 
-            var wayPoint = new WayPoint("Windisch", 47.479319847061966, 8.212966918945312);
-            Console.WriteLine("{0}: {1}/{2}", wayPoint.Name, wayPoint.Latitude, wayPoint.Longitude);
+            WayPoint wayPoint = new WayPoint("Windisch", 47.479319847061966, 8.212966918945312);
+            Console.WriteLine(wayPoint.ToString());
+            WayPoint wpBern = new WayPoint("Bern", 46.9472221, 7.451202500000022);
+            WayPoint wpTripolis = new WayPoint("Tripolis", 59.86062519999999, 17.650885199999948);
+            Console.WriteLine("Distanz Bern-Tripolis: {0}km", wpBern.Distance(wpTripolis));
 
             Console.ReadKey();
         }
