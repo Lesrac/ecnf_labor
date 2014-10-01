6b8c81476fe35ddb4ce92dbf33c144d29afd67f9
Stolz77 <stolz77@bluewin.ch>
2014-10-01 21:17:28 +0200

    Aufgabe 3

---
+++ RoutePlannerLib/RouteRequestWatcher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
+{
+    public class RouteRequestWatcher
+    {
+        public Dictionary<string, int> requests = new Dictionary<string, int>();
+
+        public int GetCityRequests(string cityName)
+        {
+            try
+            {
+                return requests[cityName];
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        public void LogRouteRequests(object sender, RouteRequestEventArgs args)
+        {
+            string toCity = args.ToCity.Name;
+            if (requests.ContainsKey(toCity))
+            {
+                requests[toCity] += 1;
+            } else {
+                requests.Add(args.ToCity.Name, 1);
+            }
+            Console.WriteLine("ToCity: {0} has been requested {1} times", toCity, requests[toCity]);
+        }
+    }
+}
