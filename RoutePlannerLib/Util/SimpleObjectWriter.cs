90599327f0cb1df0015e994b18566b4e795ad080
Roman <roman.holzner@students.fhnw.ch>
2014-10-14 14:49:36 +0200

    Implemented template

---
+++ RoutePlannerLib/Util/SimpleObjectWriter.cs
@@ -0,0 +1,22 @@
+﻿namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib.Util
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.IO;
+
+    public sealed class SimpleObjectWriter
+    {
+        public SimpleObjectWriter(TextWriter writer)
+        {
+
+        }
+
+        public void Next(object o)
+        {
+            
+        }
+    }
+}
