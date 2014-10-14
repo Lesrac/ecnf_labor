b69999c706017486b3927d53da54e8b8acebf1bc
Roman <roman.holzner@students.fhnw.ch>
2014-10-14 14:42:15 +0200

    Exercise 1 implemented

---
+++ RoutePlannerLib/RoutesFactory.cs
@@ -0,0 +1,34 @@
+﻿namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Reflection;
+
+    public class RoutesFactory
+    {
+        static public IRoutes Create(Cities cities)
+        {
+            return Create(cities,Properties.Settings.Default.RouteAlgorithm);
+        }
+
+        static public IRoutes Create(Cities cities, string algorithmClassName)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Module module = assembly.GetModules()[0];
+            Type type = module.GetType(algorithmClassName);
+            if (type == null)
+            {
+                return null;
+            }
+            ConstructorInfo ci = type.GetConstructor(new Type[] { typeof(Cities) });
+            if(ci == null) {
+                return null;
+            }
+            object factory = ci.Invoke(new object[]{cities});
+            return factory as IRoutes;
+        }
+    }
+}
